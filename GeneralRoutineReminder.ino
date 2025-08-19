24f6f05029792f776868bc1b93ec91a659b59dc9
abot8639 <abot8639@gmail.com>
2025-08-19 15:07:02 +0300

    remove loadConfiguration() + add new wifi Connecting

--- GeneralRoutineReminder.ino
+++ GeneralRoutineReminder.ino
@@ -3,14 +3,24 @@
 #include "SPIFFS.h"
 #include <time.h>
 #include <LiquidCrystal_I2C.h>
+#include <WebServer.h>  //<-- new
+
 
 #define TASKS_CAP 20
 
 
-struct Config {
-  char ssid[64];
-  char password[64];
-};
+// ---------------------WiFi------------------
+const char *ssid = ">_";
+const char *password = "Qwertyuio0qwertyuio0";
+
+// const char *ssid = "WE_F92510";
+// const char *password = ""12031302"";
+// -------------------------------------------
+
+int led = 2 ;
+bool ledState = false;
+
+WebServer server(80); 
 
 struct Time {
   int hour;
@@ -24,8 +34,8 @@ struct Task {
 
 
 // Wifi credintials
-const char* configFile = "/config.json";
-Config config;
+// const char* configFile = "/Dexconfig.json";// "/config.json"
+// Config config;
 
 // Tasks
 const char* tasksFile = "/tasks.json";
@@ -49,14 +59,16 @@ bool alarmFired = false;
 void setup() {
   
   Serial.begin(115200);
+    connectToWiFi();
+
   while (!Serial)
     continue;
 
   SPIFFS.begin();
-  loadConfiguration(configFile, config);
+  // loadConfiguration(configFile, config);
   loadTasks(tasksFile, tasks);
 
-  connectToWiFi(config.ssid, config.password);
+  // connectToWiFi(config.ssid, config.password);
 
   lcd.init();
   lcd.backlight();
@@ -111,31 +123,6 @@ void loop() {
   delay(1000);
 }
 
-
-// Loads the configuration from a file
-void loadConfiguration(const char* filename, Config& config) {
-  
-  File file = SPIFFS.open(filename);
-  if(file)
-    Serial.println("config.json is read");
-
-  JsonDocument doc;
-  DeserializationError error = deserializeJson(doc, file);
-  if (error)
-    Serial.println(F("Failed to read file"));
-  
-  strlcpy(config.ssid,          
-          doc["ssid"] | "null", 
-          sizeof(config.ssid)); 
-          
-  strlcpy(config.password,      
-          doc["password"] | "empty", 
-          sizeof(config.password));  
-
-  file.close();
-
-  Serial.println("ssid: " + String(config.ssid) + "\npassword: " + String(config.password));
-}
 
 
 // Load tasks from file
@@ -221,64 +208,7 @@ void syncTime() {
   lastNTPUpdate = millis(); // Record the time of the last sync
 }
 
-void connectToWiFi(const char*ssid, const char* password) {
-
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected.");
-}
-// ---------------------------------------------------------
-void toggleLed() {
-  Serial.print(" ledState : ");
-  Serial.println(ledState);
-
-  ledState = !ledState; 
-  digitalWrite(led, ledState ? HIGH : LOW);
-}
-
-void handleReceiveTasks() {
-
-  if (server.method() == HTTP_POST) {
-    String body = server.arg("plain"); 
-    Serial.println(" ---- JSON Received: ---- ");
-    Serial.println(body);
-
-    DynamicJsonDocument doc(1024);
-    DeserializationError error = deserializeJson(doc, body);
-    if (error) {
-      Serial.println(" XXXXX JSON parsing failed XXXXX " );
-      server.send(400, "text/plain", "Invalid JSON");
-      return;
-    }
-
-    for (JsonObject task : doc.as<JsonArray>()) {
-      Serial.println(" ");
-      String name = task["name"].as<String>();
-      int hour = task["time"]["hour"];
-      int minute = task["time"]["minute"];
-      String test = task["time"]["test"]; 
-      Serial.printf("Task: %s at %02d:%02d\n", name.c_str(), hour, minute );
-      Serial.println(test);
-      Serial.println(" ");
-      if(hour == 1){
-        toggleLed();
-      }
-
-    }
-
-    server.send(200, "text/plain", "Tasks received");
-  } else {
-    server.send(405, "text/plain", "Method Not Allowed");
-  }
-}
-
-void wifiInit(){
+void connectToWiFi() {
 
   WiFi.begin(ssid, password);
 
