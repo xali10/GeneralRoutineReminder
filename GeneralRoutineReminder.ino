12b69c095c0c888a9f66181dcbbdd4a5c26cbc01
abot8639 <abot8639@gmail.com>
2025-08-19 15:38:30 +0300

    add handleReceiveTasks

--- GeneralRoutineReminder.ino
+++ GeneralRoutineReminder.ino
@@ -67,6 +67,9 @@ void setup() {
   SPIFFS.begin();
   loadTasks(tasksFile, tasks);
 
+  server.on("/tasks", handleReceiveTasks);
+  server.begin();
+
   lcd.init();
   lcd.backlight();
 
@@ -80,7 +83,7 @@ void loop() {
   struct tm timeinfo;
   getCurrentTime(&timeinfo);
   
-  printCurrentTime(&timeinfo);
+  // printCurrentTime(&timeinfo);
 
   // Resynchronize with NTP every 30 minutes
   if (millis() - lastNTPUpdate > ntpSyncInterval) {
@@ -231,4 +234,39 @@ void pinInit(){
   pinMode(buttonPin, INPUT);
   pinMode(ringPin, OUTPUT);
   pinMode(led, OUTPUT);
+}
+void handleReceiveTasks() {
+
+  if (server.method() == HTTP_POST) {
+    String body = server.arg("plain"); 
+    Serial.println(" ---- JSON Received: ---- ");
+    Serial.println(body);
+
+    DynamicJsonDocument doc(1024);
+    DeserializationError error = deserializeJson(doc, body);
+    if (error) {
+      Serial.println(" XXXXX JSON parsing failed XXXXX " );
+      server.send(400, "text/plain", "Invalid JSON");
+      return;
+    }
+
+    for (JsonObject task : doc.as<JsonArray>()) {
+      Serial.println(" ");
+      String name = task["name"].as<String>();
+      int hour = task["time"]["hour"];
+      int minute = task["time"]["minute"];
+      String test = task["time"]["test"]; 
+      Serial.printf("Task: %s at %02d:%02d\n", name.c_str(), hour, minute );
+      Serial.println(test);
+      Serial.println(" ");
+      if(hour == 1){
+        toggleLed();
+      }
+
+    }
+
+    server.send(200, "text/plain", "Tasks received");
+  } else {
+    server.send(405, "text/plain", "Method Not Allowed");
+  }
 }
